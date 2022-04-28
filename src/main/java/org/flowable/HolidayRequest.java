42b461a91c4b7ac032e64b1c76e71ccbb6e90f10
lwmfjc <lwmfjc@gmail.com>
2022-04-28 17:29:29 +0800

    flowable官网学习

--- src/main/java/org/flowable/HolidayRequest.java
+++ src/main/java/org/flowable/HolidayRequest.java
@@ -1,15 +1,15 @@
 package org.flowable;
 
-import org.flowable.engine.ProcessEngine;
-import org.flowable.engine.ProcessEngineConfiguration;
-import org.flowable.engine.RepositoryService;
-import org.flowable.engine.RuntimeService;
+import org.flowable.engine.*;
+import org.flowable.engine.history.HistoricActivityInstance;
 import org.flowable.engine.impl.cfg.StandaloneProcessEngineConfiguration;
 import org.flowable.engine.repository.Deployment;
 import org.flowable.engine.repository.ProcessDefinition;
 import org.flowable.engine.runtime.ProcessInstance;
+import org.flowable.task.api.Task;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
 
@@ -50,8 +50,8 @@ public class HolidayRequest {
                 .singleResult();
         System.out.println("Found process definition : " + processDefinition.getName());
 
-        System.out.println("启动流程-----------");
         Scanner scanner= new Scanner(System.in);
+       /* System.out.println("启动流程-----------");
 
         System.out.println("Who are you?");
         String employee = scanner.nextLine();
@@ -62,17 +62,49 @@ public class HolidayRequest {
         System.out.println("Why do you need them?");
         String description = scanner.nextLine();
         System.out.printf("输入结果:%s--%d---%s",
-                employee,nrOfHolidays,description);
+                employee,nrOfHolidays,description);*/
 
         RuntimeService runtimeService = processEngine.getRuntimeService();
 
-        Map<String, Object> variables = new HashMap<String, Object>();
+        /*Map<String, Object> variables = new HashMap<String, Object>();
         variables.put("employee", employee);
         variables.put("nrOfHolidays", nrOfHolidays);
-        variables.put("description", description);
-        ProcessInstance processInstance =
-                runtimeService.startProcessInstanceByKey("holidayRequest", variables);
+        variables.put("description", description);*/
+        /*ProcessInstance processInstance =
+                runtimeService.startProcessInstanceByKey("holidayRequest", variables);*/
 
+        //获取经理的任务
+        TaskService taskService = processEngine.getTaskService();
+        List<Task> tasks = taskService.createTaskQuery().taskCandidateGroup("managers").list();
+        System.out.println("You have " + tasks.size() + " tasks:");
+        for (int i=0; i<tasks.size(); i++) {
+            System.out.println((i+1) + ") " + tasks.get(i).getName());
+        }
+        //选择任务并开始
+        /*System.out.println("Which task would you like to complete?");
+        int taskIndex = Integer.valueOf(scanner.nextLine());
+        Task task = tasks.get(taskIndex - 1);
+        Map<String, Object> processVariables = taskService.getVariables(task.getId());
+        System.out.println(processVariables.get("employee") + " wants " +
+                processVariables.get("nrOfHolidays") + " of holidays. Do you approve this?");
 
+        HashMap<String,Object> variables1;
+        boolean approved = scanner.nextLine().toLowerCase().equals("y");
+        variables1 = new HashMap<String, Object>();
+        variables1.put("approved", approved);
+        taskService.complete(task.getId(), variables1);*/
+        //显示
+        HistoryService historyService = processEngine.getHistoryService();
+        List<HistoricActivityInstance> activities =
+                historyService.createHistoricActivityInstanceQuery()
+                        .processInstanceId("17504")
+                        .finished()
+                        .orderByHistoricActivityInstanceEndTime().asc()
+                        .list();
+
+        for (HistoricActivityInstance activity : activities) {
+            System.out.println(activity.getActivityId() + " took "
+                    + activity.getDurationInMillis() + " milliseconds");
+        }
     }
 }
